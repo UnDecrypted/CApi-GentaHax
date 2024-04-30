a3fcb592bad76de1f4728d4cbf099f8f5b7c022d
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2024-04-30 09:28:56 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,7 +19,6 @@ capi.cinv = function(id)
 end
 
 capi.warp = function(world,path)
-  doLog("warp")
   while getWorld().name ~= world:upper() do
     sendPacket(3,"action|join_request\nname|"..world.."|"..path.."\ninvitedWorld|0")
     sleep(1000)
